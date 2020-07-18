5e6a03e8a4a57aca672124c3dfea4be75ad6085c
Niklas Fondberg <niklas.fondberg@photowall.se>
2020-07-18 22:26:08 +0200

    Make it a fixture instead

--- tests/test_client.py
+++ tests/test_client.py
@@ -15,24 +15,30 @@ def load_json_fixture(filename):
         return json.load(f)
 
 
-@pytest.mark.asyncio
-async def test_get_chargers():
-    session = aiohttp.ClientSession()
+@pytest.fixture
+def aioresponse():
     with aioresponses() as m:
-        token_data = load_json_fixture("token.json")
-        m.post(f"{BASE_URL}/api/accounts/token", payload=token_data)
+        yield m
 
-        chargers_data = load_json_fixture("chargers.json")
-        m.get(f"{BASE_URL}/api/chargers", payload=chargers_data)
 
-        easee = Easee("+46070123456", "password", session)
-        chargers = await easee.get_chargers()
-        assert chargers[0].id == "EH12345"
+@pytest.mark.asyncio
+async def test_get_chargers(aioresponse):
+    session = aiohttp.ClientSession()
 
-        chargers_state_data = load_json_fixture("charger-state.json")
-        m.get(f"{BASE_URL}/api/chargers/EH12345/state", payload=chargers_state_data)
+    token_data = load_json_fixture("token.json")
+    aioresponse.post(f"{BASE_URL}/api/accounts/token", payload=token_data)
 
-        state = await chargers[0].get_state()
-        assert state["chargerOpMode"] == "PAUSED"
-        await easee.close()
+    chargers_data = load_json_fixture("chargers.json")
+    aioresponse.get(f"{BASE_URL}/api/chargers", payload=chargers_data)
+
+    easee = Easee("+46070123456", "password", session)
+    chargers = await easee.get_chargers()
+    assert chargers[0].id == "EH12345"
+
+    chargers_state_data = load_json_fixture("charger-state.json")
+    aioresponse.get(f"{BASE_URL}/api/chargers/EH12345/state", payload=chargers_state_data)
+
+    state = await chargers[0].get_state()
+    assert state["chargerOpMode"] == "PAUSED"
+    await easee.close()
 
