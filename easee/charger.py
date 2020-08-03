804a01f7d9144bbc011e3a6010b965d7686c5be8
tmjo <54450177+tmjo@users.noreply.github.com>
2020-08-03 02:04:22 +0200

    Update charger.py

--- easee/charger.py
+++ easee/charger.py
@@ -61,33 +61,6 @@ class ChargerConfig(BaseDict):
         }
         super().__init__(data)
 
-class ChargerSchedule(BaseDict):
-    """ Charger charging schedule/plan """
-
-    def __init__(self, schedule: Dict[str, Any]):
-        data = {
-            "id": schedule.get("id", False),
-            "chargeStartTime": schedule.get("chargeStartTime", False),
-            "chargeStopTime": schedule.get("chargeStopTime", False),
-            "repeat": schedule.get("repeat", False),
-        }
-        super().__init__(data)
-
-
-
-class ChargerSchedule(BaseDict):
-    """ Charger charging schedule/plan """
-
-    def __init__(self, schedule: Dict[str, Any]):
-        # Todo remove the False defaults. Either it is a schedule object or None
-        data = {
-            "id": schedule.get("id", False),
-            "chargeStartTime": schedule.get("chargeStartTime", False),
-            "chargeStopTime": schedule.get("chargeStopTime", False),
-            "repeat": schedule.get("repeat", False),
-        }
-        super().__init__(data)
-
 
 class ChargerSchedule(BaseDict):
     """ Charger charging schedule/plan """
