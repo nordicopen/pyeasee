6ad95b85c748ac115b00e1af54d666ca1e9f23a6
Ola Lidholm <olal@olal.se>
2021-03-08 11:42:15 +0100

    bump version

--- pyeasee/easee.py
+++ pyeasee/easee.py
@@ -19,7 +19,7 @@ from .exceptions import (
 from .site import Site, SiteState
 from .utils import convert_stream_data
 
-__VERSION__ = "0.7.32"
+__VERSION__ = "0.7.33.rc1"
 
 _LOGGER = logging.getLogger(__name__)
 
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 # This call to setup() does all the work
 setup(
     name="pyeasee",
-    version="0.7.32",
+    version="0.7.33.rc1",
     description="Easee EV charger API library",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
