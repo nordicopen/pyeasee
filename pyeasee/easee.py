0d663a82ad64ce3234221a1110855b02c19bccbe
Ola Lidholm <olal@olal.se>
2022-12-18 23:39:40 +0100

    bump version

--- pyeasee/easee.py
+++ pyeasee/easee.py
@@ -22,7 +22,7 @@ from .exceptions import (
 from .site import Site, SiteState
 from .utils import convert_stream_data
 
-__VERSION__ = "0.7.49"
+__VERSION__ = "0.7.50"
 
 _LOGGER = logging.getLogger(__name__)
 
