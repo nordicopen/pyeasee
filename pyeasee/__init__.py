2424f0f36b2a48144b9736e4cf318a3779a3219e
Ola Lidholm <olal@olal.se>
2024-11-11 23:47:13 +0100

    Add version info __version__ string

--- pyeasee/__init__.py
+++ pyeasee/__init__.py
@@ -4,5 +4,6 @@ Easee charger API library
 from .charger import *  # noqa:
 from .const import *  # noqa:
 from .easee import *  # noqa:
+from .easee import __VERSION__ as __version__  # noqa:
 from .site import *  # noqa:
 from .utils import *  # noqa:
